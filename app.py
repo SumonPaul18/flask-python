fa8cdb11e50edfd69edb31141f90887faf8b8263
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-10-07 15:22:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
